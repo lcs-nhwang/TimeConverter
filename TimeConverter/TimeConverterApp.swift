6783f8bb5f9ea00123a9fcd9f714f46e7a290b77
lcs-nhwang <sjnhwang@lcs.on.ca>
2025-03-03 09:20:21 -0500

    Initial Commit

---
+++ TimeConverter/TimeConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeConverterApp.swift
+//  TimeConverter
+//
+//  Created by Nicholas Hwang on 3/3/2025.
+//
+
+import SwiftUI
+
+@main
+struct TimeConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
